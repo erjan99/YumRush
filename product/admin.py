bfc85665741631c034d8afb821d8a7b22422be7b
Your Name <erjansebat@gmail.com>
2025-08-21 16:54:06 +0600

    Models registered to admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Delivery)
+admin.site.register(Company)
 # Register your models here.
